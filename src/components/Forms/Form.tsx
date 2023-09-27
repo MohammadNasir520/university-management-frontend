40f60072d7962085f3c95071552767990d0aa04c
MohammadNasir520 <nasirahsan520@gmail.com>
2023-09-28 03:03:31 +0600

    react hook form added

---
+++ src/components/Forms/Form.tsx
@@ -0,0 +1,37 @@
+import { type } from "os";
+import { ReactElement, ReactNode } from "react";
+import { useForm, FormProvider, SubmitHandler } from "react-hook-form";
+
+type FormConfig = {
+  defaultValues?: Record<string, any>;
+};
+
+type FormProps = {
+  children?: ReactElement | ReactNode;
+  submitHandler: SubmitHandler<any>;
+} & FormConfig;
+
+type;
+
+const Form = ({ children, submitHandler, defaultValues }: FormProps) => {
+  const formConfig: FormConfig = {};
+
+  if (!!defaultValues) formConfig["defaultValues"] = defaultValues;
+
+  const methods = useForm<FormProps>(formConfig);
+
+  const { handleSubmit, reset } = methods;
+
+  const onSubmit = (data: any) => {
+    submitHandler(data);
+    reset();
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}>{children}</form>
+    </FormProvider>
+  );
+};
+
+export default Form;
--- src/components/ui/SideBar.tsx
+++ src/components/ui/SideBar.tsx
@@ -2,50 +2,11 @@
 
 import { Layout, Menu } from "antd";
 import { useState } from "react";
-import type { MenuProps } from "antd";
 
-import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
-import { SidebarItems } from "@/constants/sidebarItems";
 import { USER_ROLE } from "@/constants/role";
+import { sidebarItems } from "@/constants/sidebarItems";
 
 const { Sider } = Layout;
-
-type MenuItem = Required<MenuProps>["items"][number];
-
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[]
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem;
-}
-
-const items: MenuItem[] = [
-  getItem("Option 1", "1", <PieChartOutlined />),
-  getItem("Option 2", "2", <DesktopOutlined />),
-  getItem("User", "sub1", <UserOutlined />, [
-    getItem("Tom", "3"),
-    getItem("Bill", "4"),
-    getItem("Alex", "5"),
-  ]),
-  getItem("Team", "sub2", <TeamOutlined />, [
-    getItem("Team 1", "6"),
-    getItem("Team 2", "8"),
-  ]),
-  getItem("Files", "9", <FileOutlined />),
-];
 
 const SideBar = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -84,7 +45,7 @@ const SideBar = () => {
           theme="dark"
           defaultSelectedKeys={["1"]}
           mode="inline"
-          items={SidebarItems(role)}
+          items={sidebarItems(role)}
         />
       </Sider>
     </div>
