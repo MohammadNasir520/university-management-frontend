e2dfec5589e48c14ada6b17d053b325508be299f
MohammadNasir520 <nasirahsan520@gmail.com>
2023-09-28 05:24:42 +0600

    basic re usable from structure created

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,57 @@
+"use client";
+
+import { Button, Col, Row } from "antd";
+import Image from "next/image";
+import loginImage from "../../assets/login-image.png";
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import { SubmitHandler } from "react-hook-form";
+type FormValues = {
+  id: string;
+  password: string;
+};
+
 const page = () => {
-  return <div>this is login page</div>;
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Row>
+      <Col sm={12} md={16} lg={16}>
+        <Image src={loginImage} width={500} alt="login image"></Image>
+      </Col>
+      <Col sm={12} md={8} lg={8}>
+        <h1>First Login your account</h1>
+        <div>
+          <Form submitHandler={onSubmit}>
+            <div>
+              <FormInput
+                name="id"
+                type="text"
+                size="large"
+                label="user Id"
+              ></FormInput>
+              <FormInput
+                name="password"
+                type="password"
+                size="large"
+                label="user Password"
+              ></FormInput>
+            </div>
+
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form>
+        </div>
+      </Col>
+    </Row>
+  );
 };
 
 export default page;
