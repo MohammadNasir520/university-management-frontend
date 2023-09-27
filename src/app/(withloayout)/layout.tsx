7731a7cf76fbfac1e464617c65d373c526341246
MohammadNasir520 <nasirahsan520@gmail.com>
2023-09-28 01:57:54 +0600

    side bar done

--- src/app/(withloayout)/layout.tsx
+++ src/app/(withloayout)/layout.tsx
@@ -1,5 +1,15 @@
+import Contents from "@/components/ui/Contents";
+import SideBar from "@/components/ui/SideBar";
+import { Layout } from "antd";
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <Layout hasSider>
+      <SideBar></SideBar>
+
+      <Contents>{children}</Contents>
+    </Layout>
+  );
 };
 
 export default DashboardLayout;
